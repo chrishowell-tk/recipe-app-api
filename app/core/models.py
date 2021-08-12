78618cf71fa85432e3393795841f6b816e7c9307
Chris Howell <chris.howell@clicktravel.com>
2021-08-12 16:59:13 +0100

    Test

--- app/core/models.py
+++ app/core/models.py
@@ -16,7 +16,7 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new superuser"""
+        """Creates and saves a new superuser test"""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
