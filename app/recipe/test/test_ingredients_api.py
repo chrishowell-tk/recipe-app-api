defe1146980e08a57aab4d84546625ec0ccb7c01
Chris Howell <chris.howell@clicktravel.com>
2021-08-20 11:14:35 +0100

    Added list recipe feature

--- app/recipe/test/test_ingredients_api.py
+++ app/recipe/test/test_ingredients_api.py
@@ -13,7 +13,7 @@ from recipe.serializers import IngredientSerializer
 INGREDIENTS_URL = reverse('recipe:ingredient-list')
 
 
-class PublicIngredientsAPITest(TestCase):
+class PublicIngredientsAPITests(TestCase):
     """Test the publicly available ingredients API"""
 
     def setUp(self):
